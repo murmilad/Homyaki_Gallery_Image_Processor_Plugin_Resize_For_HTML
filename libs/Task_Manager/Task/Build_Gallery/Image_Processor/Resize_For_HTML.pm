70243e015ab7191d3a4164d7ab372238a9f6132b
Alexey Kosarev <alex@homyaki.info>
2012-03-09 00:04:38 +0400

    Initial  project version

---
+++ libs/Task_Manager/Task/Build_Gallery/Image_Processor/Resize_For_HTML.pm
@@ -0,0 +1,42 @@
+package Homyaki::Task_Manager::Task::Build_Gallery::Image_Processor::Resize_For_HTML;
+
+use strict;
+
+use Imager;
+
+use Homyaki::Imager;
+use Homyaki::Logger;
+
+use base 'Homyaki::Task_Manager::Task::Build_Gallery::Image_Processor';
+
+
+sub process {
+	my $self = shift;
+
+	my %h = @_;
+	my $image       = $h{image};
+	my $dest_path   = $h{dest_path};
+	my $source_path = $h{source_path};
+
+	my $img = rotate($image);
+
+	Homyaki::Logger::print_log("Build_Gallery: load_images: Change $source_path size:");
+
+	my $thumb = change_size($img, 64);
+	
+	unless ($thumb->write(file => $dest_path->{thumb})) {
+		Homyaki::Logger::print_log("Build_Gallery: load_images: Error: ($dest_path->{thumb})" . $img->errstr());
+		print STDERR "$dest_path->{thumb} - ",$img->errstr(),"\n";
+	}
+	
+	my $fullpic = change_size($img, 800);
+
+	unless ($fullpic->write(file=>$dest_path->{pic})) {
+		Homyaki::Logger::print_log("Build_Gallery: load_images: Error: ($dest_path->{pic})" . $img->errstr());
+		print STDERR "$dest_path->{pic} - ",$img->errstr(),"\n";
+	}
+
+	return $fullpic;
+}
+
+1;
