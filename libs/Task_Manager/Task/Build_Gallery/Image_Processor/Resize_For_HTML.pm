d0417c592e85cbe1a913f44dc84f98dc08269159
Alexey Kosarev <alex@homyaki.info>
2012-11-01 00:28:52 +0400

    Put EXIF on galiery images

--- libs/Task_Manager/Task/Build_Gallery/Image_Processor/Resize_For_HTML.pm
+++ libs/Task_Manager/Task/Build_Gallery/Image_Processor/Resize_For_HTML.pm
@@ -9,6 +9,18 @@ use Homyaki::Logger;
 
 use base 'Homyaki::Task_Manager::Task::Build_Gallery::Image_Processor';
 
+use constant EXIF_GPS_DATA_MAP => {
+		GPSLatitude         => 'GPS',
+		GPSLatitudeRef      => 'GPS',
+		GPSLongitude        => 'GPS',
+		GPSLongitudeRef     => 'GPS',
+		GPSAltitude         => 'GPS',
+		GPSAltitudeRef      => 'GPS',
+		GPSMapDatum         => 'GPS',
+		GPSImgDirection     => 'GPS',
+		GPSImgDirectionRef  => 'GPS',
+		GPSDateTime         => 'XMP'
+};
 
 sub process {
 	my $self = shift;
@@ -32,7 +44,15 @@ sub process {
 	my $fullpic = change_size($img, 800);
 
 	if ($fullpic->write(file=>$dest_path->{pic})) {
-		$self->{exif_tool}->WriteInfo($source_path, $dest_path->{pic});
+
+		my $exif_data = $self->{exif_tool}->ImageInfo($source_path);
+		if ($exif_data->{GPSLatitude}) {
+			foreach my $gps_key (keys %{&EXIF_GPS_DATA_MAP}) {
+				$self->{exif_tool}->SetNewValue($gps_key, $exif_data->{$gps_key}, &EXIF_GPS_DATA_MAP->{$gps_key});
+			}
+			$self->{exif_tool}->WriteInfo($dest_path->{pic});
+		}
+
 	} else {
 		Homyaki::Logger::print_log("Build_Gallery: load_images: Error: ($dest_path->{pic})" . $img->errstr());
 		print STDERR "$dest_path->{pic} - ",$img->errstr(),"\n";
